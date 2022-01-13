b94d9eedbb2a6cc013f4f6d7ad82d7fcdc08aeb7
kodirov8788 <kodirov8788@gmail.com>
2022-01-13 12:02:01 +0500

    lksajdljs

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
   console.log("username >>>", username);
 
   useEffect(() => {
-    setSocket(io("https://socket-io-for.herokuapp.com/"));
+    setSocket(io("http://localhost:5000"));
   }, []);
 
   useEffect(() => {
